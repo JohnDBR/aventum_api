a76f70ed768eb6d54bcae1aa03d1cc5cb54bb5a6
Juan Rodriguez <jsrd98@gmail.com>
2018-04-25 11:24:07 -0500

    [fix]: Random location

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 require 'faker'
 
 def add_journey(journey, user)
-	if(!journey.users.exists?(user.id))
+	if(!journey.users.exists?(user.id) && journey.capacity > 0)
 		journey.users << user
 		journey.update({journey_stop: journey.journey_stop + "#{user.location};", capacity: journey.capacity - 1})
 	end
 end
 
 def location
-  "#{(10.978499 + (Faker::Number.number(1).to_f / 100)).round(6)}, #{(-74.817864 - (Faker::Number.number(1).to_f / 100)).round(6)}"
+  "#{(10.978499 + (Faker::Number.number(1).to_f / 1000)).round(6)}, #{(-74.817864 - (Faker::Number.number(1).to_f / 1000)).round(6)}"
 end
 @test_student = User.create(first_name: 'Robert', last_name: 'Test', cc: '1234567899', email: 'robert@test.com', coins: 500, phone: '310675432', password: '12345', role: 'student', location: location, profile_picture: 'https://mvp-resources.s3.amazonaws.com/faeba478-9837-4c6f-a9f4-717f0ef85ebd1523736119.png')
 # @test_teacher_2 = Teacher.create(user_name: 'Brian', email: 'brianramirez482@gmail.com', phone: '1234567890', password: 12345, country: 'Colombia', city: 'Barranquilla', address: 'testtesttest', profile_picture: 'https://app-javeriana.s3.amazonaws.com/e4051da3-0f0e-46ef-afb3-6b800e1248881522190811.png', deanery: 'Math')
