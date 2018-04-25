eabca49970b8bd6b4a08c29e4a44a42c067a7d2f
Juan Rodriguez <jsrd98@gmail.com>
2018-04-25 09:47:57 -0500

    [fix]: Empty users fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require 'faker'
 
-def add_journey(journey, student)
-	journey.update({journey_stop: journey.journey_stop + "#{student.location};", capacity: journey.capacity - 1})
-	journey.users << student
+def add_journey(journey, user)
+	journey.users << user
+	journey.update({journey_stop: journey.journey_stop + "#{user.location};", capacity: journey.capacity - 1})
 end
 
-@test_student = User.create(first_name: 'Robert', last_name: 'Test', cc: '1234567899', email: 'robert@test.com', coins: 500, phone: '310675432', password: '12345', role: 'student', location: 'test', profile_picture: 'https://mvp-resources.s3.amazonaws.com/faeba478-9837-4c6f-a9f4-717f0ef85ebd1523736119.png')
+@test_student = User.create(first_name: 'Robert', last_name: 'Test', cc: '1234567899', email: 'robert@test.com', coins: 500, phone: '310675432', password: '12345', role: 'student', location: "#{11.020743 + (Faker::Number.number(2).to_f / 100)}, #{74.850721 + (Faker::Number.number(2).to_f / 100)}", profile_picture: 'https://mvp-resources.s3.amazonaws.com/faeba478-9837-4c6f-a9f4-717f0ef85ebd1523736119.png')
 # @test_teacher_2 = Teacher.create(user_name: 'Brian', email: 'brianramirez482@gmail.com', phone: '1234567890', password: 12345, country: 'Colombia', city: 'Barranquilla', address: 'testtesttest', profile_picture: 'https://app-javeriana.s3.amazonaws.com/e4051da3-0f0e-46ef-afb3-6b800e1248881522190811.png', deanery: 'Math')
 # @test_teacher_3 = Teacher.create(user_name: 'Roberto De La Vega Vallejo', email: 'rdelavega@javeriana.edu.co', phone: '1234567890', password: 12345, country: 'Colombia', city: 'Barranquilla', address: 'testtesttest', profile_picture: 'https://app-javeriana.s3.amazonaws.com/e4051da3-0f0e-46ef-afb3-6b800e1248881522190811.png', deanery: 'Math')
 # # @class_one = JavClass.create(code: '123456789', name: 'Test class teacher_one', deanery: 'tetstesttest', teacher: @test_teacher)
